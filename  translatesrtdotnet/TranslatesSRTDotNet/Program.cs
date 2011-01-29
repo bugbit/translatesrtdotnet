95f1e52c675757ff1ecb3701d861c1511c0453cc
ohernandezba <ohernandezba@b720459d-e613-ebf1-6578-9dd3585edec9>
2011-01-29 19:05:49 +0000

    Primer commit

---
+++  translatesrtdotnet/TranslatesSRTDotNet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.API.Translate;
+
+namespace TranslatesSRTDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Text = "This is a string to translate";
+            Console.WriteLine("Before Translation:{0}", Text);
+            Text = Google.API.Translate.Translator.Translate(Text, Google.API.Translate.Language.English,
+            Google.API.Translate.Language.French);
+            Console.WriteLine("Before Translation:{0}", Text);
+            Console.WriteLine();
+            Console.WriteLine("Press <ENTER> to terminate Test");
+            Console.ReadLine();
+        }
+    }
+}
