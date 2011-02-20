c83d7925569a3d528581b2b088a2390d1f307805
ohernandezba <ohernandezba@b720459d-e613-ebf1-6578-9dd3585edec9>
2011-02-20 10:46:13 +0000

    corregido bug barra process y implementado lectura ficheros .srt

--- trunk/ translatesrtdotnet/TranslatesSRTDotNet/FileSRT.cs
+++ trunk/ translatesrtdotnet/TranslatesSRTDotNet/FileSRT.cs
@@ -23,8 +23,18 @@ namespace TranslatesSRTDotNet
 {
     class FileSRT : FileSubtile
     {
+        private enum EState
+        {
+            Number,Time,Texts
+        }
+
+        private EState mState = EState.Number;
+
         /// <summary>
-        /// The SubRip file format is "perhaps the most basic of all subtitle formats."[9] SubRip files are named with the extension .srt, and contain formatted plain text. The time format used is hours:minutes:seconds,milliseconds. The decimal separator used is the comma, since the program was written in France. The line break used is often the CR+LF pair. Subtitles are numbered sequentially, starting at 1.
+        /// The SubRip file format is "perhaps the most basic of all subtitle formats."[9] SubRip files are named with the extension .srt, and 
+        /// contain formatted plain text. The time format used is hours:minutes:seconds,milliseconds. The decimal separator used is the comma, 
+        /// since the program was written in France. The line break used is often the CR+LF pair. Subtitles are numbered sequentially, 
+        /// starting at 1.
         /// Subtitle number
         /// Start time --> End time
         /// Text of subtitle (one or more lines)
@@ -34,7 +44,23 @@ namespace TranslatesSRTDotNet
         /// <returns></returns>
         protected override string TranslateLine(string argLine)
         {
-            throw new NotImplementedException();
+            switch (mState)
+            {
+                case EState.Number:
+                    mState = EState.Time;
+                    break;
+                case EState.Time:
+                    mState = EState.Texts;
+                    break;
+                case EState.Texts:
+                    if (string.IsNullOrEmpty(argLine))
+                        mState = EState.Number;
+                    else
+                        return Translate(argLine);
+                    break;
+            }
+
+            return argLine;
         }
 
         public FileSRT(string argReferrer) : base(argReferrer) { }
